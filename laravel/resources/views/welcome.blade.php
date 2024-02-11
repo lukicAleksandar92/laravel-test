d691de338c8f40ae2f98502975445923b4021738
Aleksandar Lukić <lukicaleksandar1992@gmail.com>
2024-02-11 16:01:48 +0100

    domaci_1

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+ovo je GLAVNA STRANICA
---
+++ laravel/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view("/about","about");
+
+Route::view("/shop","shop");
+
+Route::view("/contact","contact");
